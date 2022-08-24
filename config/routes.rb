238836f97495328410b6e23519b9ece674921a1e
Elena Pegoraro <elenapoikilia@gmail.com>
2022-08-24 16:48:51 +0200

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
+end
+Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurants = Restaurant.create([{ name: 'Bella Napoli', address: 'Skalitzerstraße 12', category: 'italian' },
+                                 { name: 'Tom Won', address: 'Bernauerstraße 45', category: 'chinese' },
+                                 { name: 'Mon Bijoux', address: 'Oranienstraße 1', category: 'french' },
+                                 { name: 'Chez moi', address: 'Kopernikusstraße', category: 'belgian' },
+                                 { name: 'Happy Ramen', address: 'Alexanderstraße 22', category: 'japanese' }])
